38fee058dea849d6494c1542d2a25706490f656a
ryoryoryoooo <P1211065@gmail.com>
2021-09-15 06:06:00 +0900

    switch文の中身を省略、calculationを予め初期化

--- muranakaKadai2/ViewController.swift
+++ muranakaKadai2/ViewController.swift
@@ -23,36 +23,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func calculation(_ sender: Any) {
         let num1 = Float(num1TextField.text ?? "") ?? 0
         let num2 = Float(num2TextField.text ?? "") ?? 0
+        var calculation: Float?
 
         switch selectSegmentedControl.selectedSegmentIndex {
 
         case 0:
-            let addition = num1 + num2
-            let additionRound = round(addition*10)/10
-            calculationLabel.text = String(additionRound)
+            calculation = num1 + num2
 
         case 1:
-            let subtraction = num1 - num2
-            let subtractionRound = round(subtraction*10)/10
-            calculationLabel.text = String(subtractionRound)
+            calculation = num1 - num2
 
         case 2:
-            let multiplication = num1 * num2
-            let multiplicationRound = round(multiplication*10)/10
-            calculationLabel.text = String(multiplicationRound)
+            calculation = num1 * num2
 
         case 3:
             if num2 == 0 {
                 calculationLabel.text = "割る数には０以外を入力してください"
             } else {
-                let division = num1 / num2
-                let divisionRound = round(division*10)/10
-                calculationLabel.text = String(divisionRound)
+                calculation = num1 / num2
             }
 
         default:
             break
 
         }
+        
+        let calculationRound = round((calculation ?? 0)*10)/10
+        calculationLabel.text = String(calculationRound)
     }
 }
